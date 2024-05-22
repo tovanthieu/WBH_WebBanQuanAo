04c20dfbff43270fdc35ad2bed43b7372fa67f85
tovanthieu <tovanthieu2002@gmail.com>
2024-05-22 11:19:19 +0700

    DONE

---
+++ WBH_WebBanQuanAo/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WBH_WebBanQuanAo.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
