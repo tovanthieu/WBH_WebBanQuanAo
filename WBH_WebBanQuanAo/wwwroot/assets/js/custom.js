3276c4e343ffee02814dc7f075a358edcca7837f
tovanthieu <tovanthieu2002@gmail.com>
2024-05-22 09:07:21 +0700

    Fix Font Seller

--- WBH_WebBanQuanAo/wwwroot/assets/js/custom.js
+++ WBH_WebBanQuanAo/wwwroot/assets/js/custom.js
@@ -397,42 +397,36 @@ jQuery(document).ready(function($)
 
 	*/
 
-    function initSlider()
-    {
-    	if($('.product_slider').length)
-    	{
-    		var slider1 = $('.product_slider');
+	function initSlider() {
+		if ($('.product_slider').length) {
+			var slider1 = $('.product_slider');
 
-    		slider1.owlCarousel({
-    			loop:false,
-    			dots:false,
-    			nav:false,
-    			responsive:
+			slider1.owlCarousel({
+				loop: false,
+				dots: false,
+				nav: false,
+				responsive:
 				{
-					0:{items:1},
-					480:{items:2},
-					768:{items:3},
-					991:{items:4},
-					1280:{items:5},
-					1440:{items:5}
+					0: { items: 1 },
+					480: { items: 2 },
+					768: { items: 3 },
+					991: { items: 4 },
+					1280: { items: 5 },
+					1440: { items: 5 }
 				}
-    		});
+			});
 
-    		if($('.product_slider_nav_left').length)
-    		{
-    			$('.product_slider_nav_left').on('click', function()
-    			{
-    				slider1.trigger('prev.owl.carousel');
-    			});
-    		}
+			if ($('.product_slider_nav_left').length) {
+				$('.product_slider_nav_left').on('click', function () {
+					slider1.trigger('prev.owl.carousel');
+				});
+			}
 
-    		if($('.product_slider_nav_right').length)
-    		{
-    			$('.product_slider_nav_right').on('click', function()
-    			{
-    				slider1.trigger('next.owl.carousel');
-    			});
-    		}
-    	}
-    }
+			if ($('.product_slider_nav_right').length) {
+				$('.product_slider_nav_right').on('click', function () {
+					slider1.trigger('next.owl.carousel');
+				});
+			}
+		}
+	}
 });
