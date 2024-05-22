03e91d9912de0ad3de845a8fd5f641ea5ac7496c
tovanthieu <tovanthieu2002@gmail.com>
2024-05-22 14:17:42 +0700

    giao diện login register , giao diện giỏ hàng - thông tin giỏ hàng

---
+++ WBH_WebBanQuanAo/Controllers/ShoppingCartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WBH_WebBanQuanAo.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
---
+++ WBH_WebBanQuanAo/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WBH_WebBanQuanAo.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
