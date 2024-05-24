1bfb7b74d54b52d7bc6a0e7a0b26739e1f2c746e
tovanthieu <tovanthieu2002@gmail.com>
2024-05-24 10:23:47 +0700

    update

---
+++ WBH_WebBanQuanAo/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WBH_WebBanQuanAo.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
