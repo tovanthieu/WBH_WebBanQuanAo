26a3a1f87da9cc77f502cf70732475a8a4f310de
TDP-sama <phudinhtruong69@gmail.com>
2024-05-24 14:05:02 +0700

    update

---
+++ WBH_WebBanQuanAo/Models/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace WBH_WebBanQuanAo.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ WBH_WebBanQuanAo/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace WBH_WebBanQuanAo.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Note { get; set; }
+    }
+}
