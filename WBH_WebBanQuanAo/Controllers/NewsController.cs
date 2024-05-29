edd9673306691364812e2d5304b81a9f796a28aa
TDP-sama <phudinhtruong69@gmail.com>
2024-05-29 14:20:09 +0700

    up

---
+++ WBH_WebBanQuanAo/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WBH_WebBanQuanAo.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
