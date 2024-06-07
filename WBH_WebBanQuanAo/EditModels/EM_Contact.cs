75631a032090148ed645adafcb41c680ddd765fa
TDP-sama <phudinhtruong69@gmail.com>
2024-06-07 07:45:30 +0700

    EM

---
+++ WBH_WebBanQuanAo/EditModels/EM_Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WBH_WebBanQuanAo.EditModels
+{
+	public class EM_Contact
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Tên không được để trống")]
+		public string? Name { get; set; }
+
+		[Required(ErrorMessage = "Email không được để trống")]
+		[EmailAddress(ErrorMessage = "Email không hợp lệ")]
+		public string? Email { get; set; }
+
+		[Required(ErrorMessage = "Số điện thoại không được để trống")]
+		public string? Phone { get; set; }
+
+		[Required(ErrorMessage = "Vui lòng nhập ghi chú")]
+		public string? Note { get; set; }
+	}
+}
