8aa0d9afc2fb0cc7e0ffb8badd293926e0f89bbd
tovanthieu <tovanthieu2002@gmail.com>
2024-06-07 07:56:02 +0700

    up

--- WBH_WebBanQuanAo/wwwroot/controller/product.js
+++ WBH_WebBanQuanAo/wwwroot/controller/product.js
@@ -18,3 +18,33 @@
         });
     }
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const productLinks = document.querySelectorAll('.hover-info');
+    const hoverDetails = document.querySelector('.hover-details');
+
+    productLinks.forEach(link => {
+        link.addEventListener('mouseenter', function () {
+            const image = link.getAttribute('data-image');
+            const name = link.getAttribute('data-name');
+            const description = link.getAttribute('data-description');
+            const price = link.getAttribute('data-price');
+
+            hoverDetails.innerHTML = `
+
+                        <div class="details-name">${name}</div>
+                        <div class="details-description">${description.substring(0, 200)}</div>
+                        <div class="details-price">${price}</div>
+                    `;
+            hoverDetails.style.display = 'block';
+
+            const rect = link.getBoundingClientRect();
+            hoverDetails.style.top = rect.bottom + 'px';
+            hoverDetails.style.left = rect.left + 'px';
+
+        });
+
+        link.addEventListener('mouseleave', function () {
+            hoverDetails.style.display = 'none';
+        });
+    });
+});
